e3d10c6e79ca8f82612e15d09849614cc50ba131
Miguel Pacheco <miguelpg89.mp@gmail.com>
2025-05-15 22:40:28 -0700

    se integran librerias y se crea pagina de inicio

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Sistema de Gestión</title>
+    <link rel="stylesheet" href="assets/libs/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+  </head>
+  <body>
+    <header class="bg-light py-3 px-4 shadow-sm fixed-top">
+      <div class="container-fluid">
+        <span class="header-title">
+          <span class="grupo">Grupo</span> <span class="impulsora">Impulsora</span>
+        </span>
+      </div>
+    </header>
+    <main  class="d-flex justify-content-center align-items-center vh-100">
+      <div class="container text-center">
+        <h1 class="title fw-bold">Bienvenido al Sistema de Gestión de Empleados y Equipos</h1>
+        <div class="row justify-content-center mt-5">
+          <div class="col-12 col-md-6 col-lg-4 mb-4">
+            <a href="empleados/listar.php" class="text-decoration-none text-dark">
+              <div class="card card-option text-center p-4 bg-success">
+                <div class="icon-box mx-auto mb-3">👥</div>
+                <h4 class="card-title">Gestión de Empleados</h4>
+              </div>
+            </a>
+          </div>
+          <div class="col-12 col-md-6 col-lg-4 mb-4">
+            <a href="equipos/listar.php" class="text-decoration-none text-dark">
+              <div class="card card-option text-center p-4 bg-warning">
+                <div class="icon-box mx-auto mb-3">💻</div>
+                <h4 class="card-title">Gestión de Equipos</h4>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </main >
+    <script src="assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/libs/sweetalert2/sweetalert2.all.min.js"></script>
+  </body>
+</html>
