c62fc4eaa5119310c05b05cc5161bf581907a31b
Miguel Pacheco <miguelpg89.mp@gmail.com>
2025-05-16 23:00:30 -0700

    se sube base de datos y consumo para listar empleados y equipos

--- equipos/listar.php
+++ equipos/listar.php
@@ -1,84 +1,28 @@
-<!DOCTYPE html>
-<html lang="es">
-    <head>
-        <meta charset="UTF-8">
-        <title>Lista de Equipos</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="../assets/libs/bootstrap/css/bootstrap.min.css">
-        <link rel="stylesheet" href="../assets/css/style.css">
-        <script src="../assets/libs/sweetalert2/sweetalert2.all.min.js"></script>
-    </head>
-    <body>
-        <header class="bg-light py-3 px-4 shadow-sm">
-            <div class="container-fluid">
-                <div class="header-title">
-                    <span class="grupo">Grupo</span> <span class="impulsora">Impulsora</span>
-                </div>
-            </div>
-        </header>
-        <main class="container mt-5 mb-5">
-            <div class="d-flex justify-content-between align-items-center mb-4">
-                <h2 class="fw-bold text-success">Lista de Equipos</h2>
-                <a href="crear.php" class="btn btn-success">+ Agregar Equipo</a>
-            </div>
+<?php
+    header('Content-Type: application/json');
+    require_once '../db/conexion.php';
 
-            <table class="table table-bordered table-hover shadow-sm">
-                <thead class="table-success">
-                    <tr>
-                    <th>ID</th>
-                    <th>Nombre Equipo</th>
-                    <th>Tipo</th>
-                    <th>Serie</th>
-                    <th>Empleado Asignado</th>
-                    <th>Acciones</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>Lenovo</td>
-                        <td>LapTop</td>
-                        <td>12345</td>
-                        <td>Miguel Pacheco</td>
-                        <td>
-                            <a href="editar.php?id=1" class="btn btn-sm btn-warning">Editar</a>
-                            <button class="btn btn-sm btn-danger" onclick="confirmarEliminacion(1)">Eliminar</button>
-                        </td>
-                    </tr>
-                        <tr>
-                        <td>2</td>
-                        <td>MAC</td>
-                        <td>PC Escritorio</td>
-                        <td>67890</td>
-                        <td>Melissa Willis</td>
-                        <td>
-                            <a href="editar.php?id=2" class="btn btn-sm btn-warning">Editar</a>
-                            <button class="btn btn-sm btn-danger" onclick="confirmarEliminacion(2)">Eliminar</button>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-            <div class="d-flex justify-content-end  mt-4">
-                <a href="../index.php" class="btn btn-success">Regresar inicio</a>
-            </div>
-        </main>
+    $conexion = (new Conexion())->conectar();
 
-        <script>
-            function confirmarEliminacion(id) {
-            Swal.fire({
-                title: '¿Eliminar empleado?',
-                icon: 'warning',
-                showCancelButton: true,
-                confirmButtonText: 'Sí, eliminar',
-                confirmButtonColor: "#00A44D",
-                cancelButtonColor: "#d33",
-                cancelButtonText: 'Cancelar'
-            }).then((result) => {
-                if (result.isConfirmed) {
-                window.location.href = 'eliminar.php?id=' + id;
-                }
-            });
-            }
-        </script>
-    </body>
-</html>
+    $sql = "SELECT 
+                e.idEquipo,
+                e.nombreEquipo,
+                t.nombreTipo,
+                e.serie,
+                (ISNULL(emp.nombreEmpleado, '') + ' ' + ISNULL(emp.apellidoPaterno, '') + ' ' + ISNULL(emp.apellidoMaterno, '')) AS responsable
+            FROM equipos e
+            INNER JOIN tipoEquipo t ON e.idTipo = t.idTipo
+            INNER JOIN empleados emp ON e.responsableId = emp.idEmpleado;";
+
+    $stmt = $conexion->query($sql);
+
+    $equipos = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    foreach ($equipos as &$equipo) {
+        $equipo['nombreEquipo'] = mb_convert_encoding($equipo['nombreEquipo'], 'UTF-8', 'ISO-8859-1');
+        $equipo['serie'] = mb_convert_encoding($equipo['serie'], 'UTF-8', 'ISO-8859-1');
+        $equipo['responsable'] = mb_convert_encoding($equipo['responsable'], 'UTF-8', 'ISO-8859-1');
+        $equipo['nombreTipo'] = mb_convert_encoding($equipo['nombreTipo'], 'UTF-8', 'ISO-8859-1');
+    }   
+    echo json_encode($equipos);
+?>
--- index.php
+++ index.php
@@ -8,37 +8,174 @@
     <link rel="stylesheet" href="assets/css/style.css">
   </head>
   <body>
-    <header class="bg-light py-3 px-4 shadow-sm fixed-top">
+    <header class="bg-light py-3 px-4 shadow-sm">
       <div class="container-fluid">
         <div class="header-title">
-          <span class="grupo">Grupo</span> <span class="impulsora">Impulsora</span>
-        </div>
+            <span class="grupo text-warning">Grupo</span> <span class="impulsora text-success">Impulsora</span>
+        </span>
       </div>
     </header>
-    <main  class="d-flex justify-content-center align-items-center vh-100">
-      <div class="container text-center">
-        <h1 class="title fw-bold">Bienvenido al Sistema de Gestión de Empleados y Equipos</h1>
-        <div class="row justify-content-center mt-5">
-          <div class="col-12 col-md-6 col-lg-4 mb-4">
-            <a href="empleados/listar.php" class="text-decoration-none text-dark">
-              <div class="card card-option text-center p-4 bg-success">
-                <div class="icon-box mx-auto mb-3">👥</div>
-                <h4 class="card-title">Gestión de Empleados</h4>
-              </div>
-            </a>
+
+    <div class="container mt-5">
+      <div id="opciones" class="row justify-content-center">
+        <div class="col-12 col-md-6 col-lg-4 mb-4" onclick="mostrarTabla('empleados')">
+          <div class="card card-option text-center p-4 bg-success">
+            <div class="icon-box mx-auto mb-3">👥</div>
+            <h4 class="card-title">Gestión de Empleados</h4>
           </div>
-          <div class="col-12 col-md-6 col-lg-4 mb-4">
-            <a href="equipos/listar.php" class="text-decoration-none text-dark">
-              <div class="card card-option text-center p-4 bg-warning">
-                <div class="icon-box mx-auto mb-3">💻</div>
-                <h4 class="card-title">Gestión de Equipos</h4>
-              </div>
-            </a>
+        </div>
+        <div class="col-12 col-md-6 col-lg-4 mb-4" onclick="mostrarTabla('equipos')">
+          <div class="card card-option text-center p-4 bg-warning">
+            <div class="icon-box mx-auto mb-3">💻</div>
+            <h4 class="card-title">Gestión de Equipos</h4>
           </div>
         </div>
       </div>
-    </main >
+
+      <div id="tablaContenido" class="mt-5 motrarTabla">
+        <div class="d-flex justify-content-between mb-3">
+          <h2 id="tituloTabla" class="fw-bold text-success"></h2>
+          <button class="btn btn-success" onclick="inicio()">Volver</button>
+        </div>
+        <table class="table table-bordered table-hover">
+          <thead id="tablaHead" class="table-success"></thead>
+          <tbody id="tablaBody"></tbody>
+        </table>
+        <div id="paginador" class="text-center mt-3"></div>
+      </div>
+    </div>
+
+    <script>
+      function inicio() {
+        document.getElementById('tablaContenido').classList.add('motrarTabla');
+        document.getElementById('opciones').classList.remove('motrarTabla');
+      }
+
+      function mostrarTabla(tipo) {
+        document.getElementById('opciones').classList.add('motrarTabla');
+        document.getElementById('tablaContenido').classList.remove('motrarTabla');
+
+        const titulo = document.getElementById('tituloTabla');
+        const tablaHead = document.getElementById('tablaHead');
+
+        if (tipo === 'empleados') {
+          titulo.textContent = 'Lista de Empleados';
+          tablaHead.innerHTML = `
+            <tr>
+              <th>
+                ID
+                <input type="text" class="form-control form-control-sm mt-1" placeholder="Buscar ID" onkeyup="filtrarTabla(0, this.value)">
+              </th>
+              <th>
+                Nombre
+                <input type="text" class="form-control form-control-sm mt-1" placeholder="Buscar nombre" onkeyup="filtrarTabla(1, this.value)">
+              </th>
+              <th>
+                Correo
+                <input type="text" class="form-control form-control-sm mt-1" placeholder="Buscar correo" onkeyup="filtrarTabla(2, this.value)">
+              </th>
+              <th>
+                Departamento
+                <input type="text" class="form-control form-control-sm mt-1" placeholder="Buscar depto." onkeyup="filtrarTabla(3, this.value)">
+              </th>
+              <th>Acciones</th>
+            </tr>
+          `;
+          cargarEmpleados();
+        } else if (tipo === 'equipos') {
+          titulo.textContent = 'Lista de Equipos';
+          tablaHead.innerHTML = `
+            <tr>
+              <th>
+                ID
+                <input type="text" class="form-control form-control-sm mt-1" placeholder="Buscar ID" onkeyup="filtrarTabla(0, this.value)">
+              </th>
+              <th>
+                Nombre Equipo
+                <input type="text" class="form-control form-control-sm mt-1" placeholder="Buscar Nombre Equipo" onkeyup="filtrarTabla(1, this.value)">
+              </th>
+              <th>
+                Tipo
+                <input type="text" class="form-control form-control-sm mt-1" placeholder="Buscar Tipo" onkeyup="filtrarTabla(2, this.value)">
+              </th>
+              <th>
+                Serie
+                <input type="text" class="form-control form-control-sm mt-1" placeholder="Buscar Serie" onkeyup="filtrarTabla(3, this.value)">
+              </th>
+              <th>
+                Empleado Asignado
+                <input type="text" class="form-control form-control-sm mt-1" placeholder="Buscar Empleado" onkeyup="filtrarTabla(4, this.value)">
+              </th>
+              <th>Acciones</th>
+            </tr>
+          `;
+          cargarEquipos();
+        }
+      }
+
+      function cargarEmpleados() {
+        fetch('empleados/listar.php')
+          .then(response => response.json())
+          .then(data => {
+            const empleados = document.getElementById('tablaBody');
+            empleados.innerHTML = '';
+
+            data.forEach(e => {
+              const fila = `
+                <tr>
+                  <td>${e.idEmpleado}</td>
+                  <td>${e.nombreEmpleado}</td>
+                  <td>${e.correo}</td>
+                  <td>${e.nombreDepartamento}</td>
+                  <td>
+                    <a href="editar.php?id=2" class="btn btn-sm btn-warning">Editar</a>
+                    <button class="btn btn-sm btn-danger" onclick="confirmarEliminacion(2)">Eliminar</button>
+                  </td>
+                </tr>`;
+              empleados.innerHTML += fila;
+            });
+          })
+        .catch(error => console.error('Error al cargar empleados:', error));
+      }
+
+      function cargarEquipos() {
+        fetch('equipos/listar.php')
+          .then(response => response.json())
+          .then(data => {
+            const empleados = document.getElementById('tablaBody');
+            console.log(data)
+            empleados.innerHTML = '';
+
+            data.forEach(e => {
+              const fila = `
+                <tr>
+                  <td>${e.idEquipo}</td>
+                  <td>${e.nombreEquipo}</td>
+                  <td>${e.nombreTipo}</td>
+                  <td>${e.serie}</td>
+                  <td>${e.responsable}</td>
+                  <td>
+                    <a href="editar.php?id=2" class="btn btn-sm btn-warning">Editar</a>
+                    <button class="btn btn-sm btn-danger" onclick="confirmarEliminacion(2)">Eliminar</button>
+                  </td>
+                </tr>`;
+              empleados.innerHTML += fila;
+            });
+          })
+        .catch(error => console.error('Error al cargar equipos:', error));
+      }
+
+      function filtrarTabla(columna, cadena) {
+        cadena = cadena.toLowerCase();
+        const filas = document.querySelectorAll("table tbody tr");
+
+        filas.forEach(fila => {
+            const celda = fila.cells[columna];
+            const texto = celda.textContent.toLowerCase();
+            fila.style.display = texto.includes(cadena) ? "" : "none";
+        });
+      }
+    </script>
     <script src="assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="assets/libs/sweetalert2/sweetalert2.all.min.js"></script>
   </body>
 </html>
